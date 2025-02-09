d4cf1d0359e0e3b772734ded723ee3e3bb93d1fc
Jan Zemlicka <jan.zemlicka1@gmail.com>
2025-02-09 16:23:38 +0000

    Configured database connection in appsettings.json, created database context and installed NuGet packages.

---
+++ MusicAPI/MusicApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicAPI.Models;
+
+namespace MusicAPI;
+
+public class MusicApiContext : DbContext
+{
+    public MusicApiContext(DbContextOptions<MusicApiContext> options) : base(options) { }
+
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Album> Albums { get; set; }
+    public DbSet<Song> Songs { get; set; }
+}
